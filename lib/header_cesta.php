fec58361d1092a2991cccfded47ed336c0307b7c
Rafa Montes <rafamontes88@gmail.com>
2022-01-17 23:16:31 +0100

    Versión 0.1.2

--- lib/header_cesta.php
+++ lib/header_cesta.php
@@ -3,7 +3,9 @@
     <div><a href="index.php"><i class="fas fa-carrot w3-jumbo" style="color:orange;"></i></a></div>
 <h1>Frutas Pili</h1>
 <div id="cesta" class="w3-dropdown-hover w3-right">
-    <button class="w3-button w3-green"><span class="w3-badge w3-red w3-xlarge"><?=count($_COOKIE['frutas']);?></span>  <i class="fa fa-shopping-basket w3-xxlarge"></i></button>
+    <button class="w3-button w3-green"><span class="w3-badge w3-red w3-xlarge"><?php
+    echo (count($_COOKIE['frutas']));?>
+  </span> <!-- <i class="fa fa-shopping-basket w3-xxlarge"></i></button>-->
     <div class="w3-dropdown-content w3-bar-block w3-border">
       <a href="void.php" class="w3-bar-item w3-button">Vaciar</a>
 </div>
