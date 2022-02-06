80f1c7fb0023e53cd777c13ee213f64410eb670b
Rafa Montes <rafamontes88@gmail.com>
2022-02-06 03:27:22 +0100

    Delete logout.php

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-    
-    setcookie("usuario",$_SESSION['nombre'],time()-60*60*24*365);
-    
-    session_destroy();
-    $_SESSION = array();
-    header("Location:index.php");
-?>
